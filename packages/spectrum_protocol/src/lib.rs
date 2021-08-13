911d22a54eb6883a71ed874dd79d4c7688a130ac
Peiqian Li <peiqian@aperture.finance>
2021-08-12 20:11:43 -0700

    Add spectrum_protocol package to repo as it is not available in crates.io index.

---
+++ packages/spectrum_protocol/src/lib.rs
@@ -0,0 +1,13 @@
+#![allow(non_camel_case_types)]
+
+pub mod common;
+pub mod gov;
+pub mod math;
+pub mod mirror_farm;
+pub mod platform;
+pub mod querier;
+pub mod spec_farm;
+pub mod staker;
+pub mod wallet;
+pub mod anchor_farm;
+pub mod pylon_farm;
