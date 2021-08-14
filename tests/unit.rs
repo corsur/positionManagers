a1bdfd45bd9e94cd06c3f7dcdfeede7091b1220b
Peiqian Li <peiqian@aperture.finance>
2021-08-13 20:30:01 -0700

    Expand authorization test.

--- tests/unit.rs
+++ tests/unit.rs
@@ -1,6 +1,6 @@
 use cosmwasm_std::testing::{mock_dependencies, mock_env};
 use cosmwasm_std::{
-    coins, from_binary, HandleResponse, HandleResult, HumanAddr, StdError,
+    coins, from_binary, HandleResponse, HandleResult, HumanAddr, StdError, StdResult, Uint128,
 };
 
 use amadeus::contract::{handle, init};
@@ -20,9 +20,38 @@ fn mock_init_msg() -> InitMsg {
     }
 }
 
+fn mock_claim_short_sale_proceeds_and_stake(stake_via_spectrum: bool) -> HandleMsg {
+    HandleMsg::ClaimShortSaleProceedsAndStake {
+        cdp_idx: Uint128::from(1000u128),
+        mirror_asset_amount: Uint128::from(1000000u128),
+        stake_via_spectrum,
+    }
+}
+
+fn mock_close_short_position() -> HandleMsg {
+    HandleMsg::CloseShortPosition {
+        cdp_idx: Uint128::from(1000u128),
+    }
+}
+
+fn mock_delta_neutral_invest() -> HandleMsg {
+    HandleMsg::DeltaNeutralInvest {
+        collateral_asset_amount: Uint128::from(1000000u128),
+        collateral_ratio_in_percentage: Uint128::from(200u128),
+        mirror_asset_to_mint_cw20_addr: HumanAddr::from("mirror_asset_cw20"),
+    }
+}
+
 fn mock_do_msg() -> HandleMsg {
     HandleMsg::Do {
         cosmos_messages: vec![],
+    }
+}
+
+fn assert_unauthorized_error(res: StdResult<HandleResponse>) {
+    match res {
+        Err(StdError::Unauthorized { .. }) => {}
+        _ => panic!("Expecting StdError::Unauthorized."),
     }
 }
 
@@ -32,13 +61,15 @@ fn authorization() {
     let env = mock_env(/*sender=*/ "owner", &[]);
 
     // We can just call .unwrap() to assert this was a success.
-    let res = init(&mut deps, env, mock_init_msg()).unwrap();
+    let res = init(&mut deps, env.clone(), mock_init_msg()).unwrap();
     assert_eq!(0, res.messages.len());
 
     let unauthorized_env = mock_env(/*sender=*/ "anyone", &[]);
-    let res = handle(&mut deps, unauthorized_env, mock_do_msg());
-    match res {
-        Err(StdError::Unauthorized { .. }) => {}
-        _ => panic!("Must return StdError::Unauthorized."),
-    }
+    assert_unauthorized_error(handle(&mut deps, unauthorized_env.clone(), mock_claim_short_sale_proceeds_and_stake(false)));
+    assert_unauthorized_error(handle(&mut deps, unauthorized_env.clone(), mock_close_short_position()));
+    assert_unauthorized_error(handle(&mut deps, unauthorized_env.clone(), mock_delta_neutral_invest()));
+    assert_unauthorized_error(handle(&mut deps, unauthorized_env, mock_do_msg()));
+
+    // Assert that owner can successfully execute an empty Do call.
+    let _res = handle(&mut deps, env, mock_do_msg()).unwrap();
 }
