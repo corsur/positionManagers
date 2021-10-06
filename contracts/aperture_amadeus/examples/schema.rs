3e3d143c637679896eb33d6b5571ea4c3b95ad2a
Peiqian Li <peiqian@aperture.finance>
2021-10-05 22:18:11 -0700

    Fix schema issue due to package renaming.

--- contracts/aperture_amadeus/examples/schema.rs
+++ contracts/aperture_amadeus/examples/schema.rs
@@ -3,8 +3,8 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use amadeus::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
-use amadeus::state::Config;
+use aperture_amadeus::msg::{ExecuteMsg, InstantiateMsg, MigrateMsg, QueryMsg};
+use aperture_amadeus::state::Config;
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
