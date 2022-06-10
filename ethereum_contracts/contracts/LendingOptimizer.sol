c2e8d20d80013d1b393f718fd9b1132ada827c04
Andrew Chang <yc16036@gmail.com>
2022-06-10 11:14:12 -0700

    Lending optimizer - supply to aave (#80)
    * goal #1: supply token (USDC) to compound
    
    * updates for pr#79
    
    * 2nd update pr#79
    
    change mint to transfer in comment
    
    * supplyToAave and corresponding unit tests finished
    
    * comments addressed for pr #80

--- ethereum_contracts/contracts/LendingOptimizer.sol
+++ ethereum_contracts/contracts/LendingOptimizer.sol
@@ -17,10 +17,19 @@ interface CErc20 {
     function redeemUnderlying(uint256) external returns (uint256);
 }
 
+interface ILendingPool {
+    function deposit(
+        address asset,
+        uint256 amount,
+        address onBehalfOf,
+        uint16 referralCode
+    ) external;
+}
+
 contract LendingOptimizer {
     using SafeERC20 for IERC20;
 
-    function supplyTokenToCompound(uint256 amount) external returns (uint256) {
+    function supplyTokenToCompound(uint256 amount) external {
         address addr = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; // USDC address
         address cAddr = 0x39AA39c021dfbaE8faC545936693aC917d5E7563; // cUSDC address
 
@@ -33,11 +42,29 @@ contract LendingOptimizer {
         // approve compound contract to transfer from this contract
         token.safeApprove(cAddr, amount);
 
-        return cToken.mint(amount);
+        cToken.mint(amount);
     }
 
-    function supplyTokenToAave(address token, uint256 amount) external {
+    function supplyTokenToAave(uint256 amount) external {
         address addr = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; // USDC address
+        address aaveLendingPoolAddr = 0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9;
+
+        IERC20 token = IERC20(addr);
+        ILendingPool pool = ILendingPool(aaveLendingPoolAddr);
+
+        // approve and transfer tokens from investor wallet to this contract
+        token.safeTransferFrom(msg.sender, address(this), amount);
+
+        // approve AAVE LendingPool contract to make a deposit
+        token.safeApprove(aaveLendingPoolAddr, amount);
+
+        pool.deposit(
+            addr,
+            amount,
+            address(this),
+            /*referralCode=*/
+            0
+        );
     }
 
     function supply(address token, uint256 amount) external {}
--- ethereum_contracts/test/lending_optimizer.js
+++ ethereum_contracts/test/lending_optimizer.js
@@ -1,7 +1,8 @@
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
 const { Web3Provider } = require("@ethersproject/providers");
-const { cTokenAbi } = require('./cTokenABI.js');
+const { cTokenAbi } = require('./abi/cTokenABI.js');
+const { aTokenAbi } = require('./abi/aTokenABI.js');
 
 const erc20ABI = [
   // Read-Only Functions
@@ -13,6 +14,7 @@ const erc20ABI = [
 const IMPERSONATE_ADDR = "0x2FAF487A4414Fe77e2327F0bf4AE2a264a776AD2";
 const USDC_ADDR = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
 const CUSDC_ADDR = "0x39AA39c021dfbaE8faC545936693aC917d5E7563";
+const AUSDC_ADDR = "0xBcca60bB61934080951369a648Fb03DF4F96263C";
 
 async function getImpersonatedSigner() {
   const accountToImpersonate = IMPERSONATE_ADDR;
@@ -64,6 +66,31 @@ async function testSupplyTokenToCompound(signer, lendingOptimizer) {
   return [signerBalanceDelta, compoundBalanceDelta, cDelta];
 }
 
+async function testSupplyTokenToAave(signer, lendingOptimizer) {
+  const supplyAmount = 20 * 1e6; // $20
+  const tokenContract = new ethers.Contract(USDC_ADDR, erc20ABI, signer);
+  const aTokenContract = new ethers.Contract(AUSDC_ADDR, aTokenAbi, signer);
+
+  await tokenContract.approve(lendingOptimizer.address, supplyAmount);
+
+  const prevSignerBalance = await tokenContract.balanceOf(signer.address);
+  const prevAaveBalance = await tokenContract.balanceOf(AUSDC_ADDR);
+  const prevABalance = await aTokenContract.balanceOf(lendingOptimizer.address);
+
+  await lendingOptimizer.supplyTokenToAave(supplyAmount);
+
+  const afterSignerBalance = await tokenContract.balanceOf(signer.address);
+  const afterAaveBalance = await tokenContract.balanceOf(AUSDC_ADDR);
+  const afterABalance = await aTokenContract.balanceOf(lendingOptimizer.address);
+
+  const signerBalanceDelta = (afterSignerBalance - prevSignerBalance) / 1e6;
+  const aaveBalanceDelta = (afterAaveBalance - prevAaveBalance) / 1e6;
+  const aDelta = (afterABalance - prevABalance) / 1e6;
+
+  console.log("supplyTokenToAave(supplyAmount) completed.");
+  return [signerBalanceDelta, aaveBalanceDelta, aDelta];
+}
+
 describe.only("Lending optimizer supply to compound unit tests", function () {
   var signer = undefined;
   var lendingOptimizer = undefined;
@@ -73,10 +100,17 @@ describe.only("Lending optimizer supply to compound unit tests", function () {
     lendingOptimizer = await deployLendingOptimizer(signer);
   });
 
-  it("Supply to compound balance check", async function () {
+  it("Supply to Compound balance check", async function () {
     const [signerBalanceDelta, compoundBalanceDelta, cDelta] = await testSupplyTokenToCompound(signer, lendingOptimizer);
     expect(signerBalanceDelta).to.equal(-20);
     expect(compoundBalanceDelta).to.equal(20);
     expect(Math.ceil(cDelta)).to.equal(20); // ceil because some decimals are lost with exchange rate multiplication
+  });
+
+  it("Supply to AAVE balance check", async function () {
+    const [signerBalanceDelta, aaveBalanceDelta, aDelta] = await testSupplyTokenToAave(signer, lendingOptimizer);
+    expect(signerBalanceDelta).to.equal(-20);
+    expect(aaveBalanceDelta).to.equal(20);
+    expect(aDelta).to.equal(20);
   });
 });
