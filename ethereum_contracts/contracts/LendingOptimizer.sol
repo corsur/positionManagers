df85782418c7e3b5f96aaca649160a4e90060bcc
Andrew Chang <yc16036@gmail.com>
2022-06-15 12:00:04 -0700

    pr#83 comments addressed

--- ethereum_contracts/contracts/LendingOptimizer.sol
+++ ethereum_contracts/contracts/LendingOptimizer.sol
@@ -2,6 +2,10 @@ pragma solidity ^0.8.13;
 
 import "hardhat/console.sol";
 
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
@@ -72,55 +76,39 @@ interface WETHGateway {
     ) external payable;
 }
 
-contract LendingOptimizer {
+contract LendingOptimizer is
+    Initializable,
+    UUPSUpgradeable,
+    OwnableUpgradeable
+{
     using SafeERC20 for IERC20;
 
     mapping(address => address) toC;
 
-    function toCAddr() private {
-        // SUSHI, COMP not included because not on aave
-        toC[
-            0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9
-        ] = 0xe65cdB6479BaC1e22340E4E755fAE7E509EcD06c; // AAVE
-        toC[
-            0x0D8775F648430679A709E98d2b0Cb6250d2887EF
-        ] = 0x6C8c6b02E7b2BE14d4fA6022Dfd6d75921D90E4E; // BAT
-        toC[
-            0x6B175474E89094C44Da98b954EedeAC495271d0F
-        ] = 0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643; // DAI
-        toC[
-            0x956F47F50A910163D8BF957Cf5846D573E7f87CA
-        ] = 0x7713DD9Ca933848F6819F38B8352D9A15EA73F67; // FEI
-        toC[
-            0x514910771AF9Ca656af840dff83E8264EcF986CA
-        ] = 0xFAce851a4921ce59e912d19329929CE6da6EB0c7; // LINK
-        toC[
-            0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2
-        ] = 0x95b4eF2869eBD94BEb4eEE400a99824BF5DC325b; // MKR
-        toC[
-            0x0000000000085d4780B73119b644AE5ecd22b376
-        ] = 0x12392F67bdf24faE0AF363c24aC620a2f67DAd86; // TUSD
-        toC[
-            0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984
-        ] = 0x35A18000230DA775CAc24873d00Ff85BccdeD550; // UNI
-        toC[
-            0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48
-        ] = 0x39AA39c021dfbaE8faC545936693aC917d5E7563; // USDC
-        toC[
-            0x8E870D67F660D95d5be530380D0eC0bd388289E1
-        ] = 0x041171993284df560249B57358F931D9eB7b925D; // USDP
-        toC[
-            0xdAC17F958D2ee523a2206206994597C13D831ec7
-        ] = 0xf650C3d88D12dB855b8bf7D11Be6C55A4e07dCC9; // USDT
-        toC[
-            0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599
-        ] = 0xC11b1268C1A384e55C48c2391d8d480264A3A7F4; // WBTC
-        toC[
-            0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e
-        ] = 0x80a2AE356fc9ef4305676f7a3E2Ed04e12C33946; // YFI
-        toC[
-            0xE41d2489571d322189246DaFA5ebDe1F4699F498
-        ] = 0xB3319f5D18Bc0D84dD1b4825Dcde5d5f7266d407; // ZRX
+    address public CETH_ADDR; // 0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5 on ethereum
+    address public ILENDINGPOOL_ADDR; // 0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9 on ethereum
+    address public WETH_ADDR; // 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2 on ethereum
+    address public WETHGATEWAY_ADDR; // 0xcc9a0B7c43DC2a5F023Bb9b738E45B0Ef6B06E04 on ethereum
+
+    function initialize(
+        address _cETHAddr,
+        address _lendingPoolAddr,
+        address _wethAddr,
+        address _wethGatewayAddr
+    ) public initializer {
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+        CETH_ADDR = _cETHAddr;
+        ILENDINGPOOL_ADDR = _lendingPoolAddr;
+        WETH_ADDR = _wethAddr;
+        WETHGATEWAY_ADDR = _wethGatewayAddr;
+    }
+
+    // Only owner of this logic contract can upgrade.
+    function _authorizeUpgrade(address) internal override onlyOwner {}
+
+    function mapToC(address tokenAddr, address cTokenAddr) external onlyOwner {
+        toC[tokenAddr] = cTokenAddr;
     }
 
     function supplyTokenToCompound(address tokenAddr, uint256 amount) private {
@@ -128,7 +116,7 @@ contract LendingOptimizer {
         CErc20 cToken = CErc20(toC[tokenAddr]);
 
         // approve and transfer tokens from investor wallet to this contract
-        token.safeTransferFrom(tx.origin, address(this), amount);
+        token.safeTransferFrom(msg.sender, address(this), amount);
 
         // approve compound contract to transfer from this contract
         token.safeApprove(toC[tokenAddr], amount);
@@ -138,15 +126,13 @@ contract LendingOptimizer {
 
     function supplyTokenToAave(address tokenAddr, uint256 amount) private {
         IERC20 token = IERC20(tokenAddr);
-        ILendingPool pool = ILendingPool(
-            0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9
-        ); // address is AAVE LendingPool
+        ILendingPool pool = ILendingPool(ILENDINGPOOL_ADDR); // address is AAVE LendingPool
 
         // approve and transfer tokens from investor wallet to this contract
-        token.safeTransferFrom(tx.origin, address(this), amount);
+        token.safeTransferFrom(msg.sender, address(this), amount);
 
         // approve AAVE LendingPool contract to make a deposit
-        token.safeApprove(0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9, amount);
+        token.safeApprove(ILENDINGPOOL_ADDR, amount);
 
         pool.deposit(
             tokenAddr,
@@ -158,22 +144,18 @@ contract LendingOptimizer {
     }
 
     function supplyEth() external payable {
-        CEth cToken = CEth(0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5); // cETH
+        CEth cToken = CEth(CETH_ADDR); // cETH
         uint256 cInterestAdj = cToken.supplyRatePerBlock() *
             6570 *
             365 *
             (10**9);
 
-        ILendingPool pool = ILendingPool(
-            0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9
-        );
+        ILendingPool pool = ILendingPool(ILENDINGPOOL_ADDR);
         uint256 aInterestAdj = pool
-            .getReserveData(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2)
+            .getReserveData(WETH_ADDR)
             .currentLiquidityRate;
 
-        WETHGateway wETH = WETHGateway(
-            0xcc9a0B7c43DC2a5F023Bb9b738E45B0Ef6B06E04
-        );
+        WETHGateway wETH = WETHGateway(WETHGATEWAY_ADDR);
 
         // console.log(cInterestAdj);
         // console.log(aInterestAdj);
@@ -182,7 +164,7 @@ contract LendingOptimizer {
             cToken.mint{value: msg.value}();
         } else {
             wETH.depositETH{value: msg.value}(
-                0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9,
+                ILENDINGPOOL_ADDR,
                 address(this),
                 /* referralCode = */
                 0
@@ -195,18 +177,29 @@ contract LendingOptimizer {
         external
         returns (uint256)
     {
-        toCAddr();
-
-        require(toC[tokenAddr] != 0x0000000000000000000000000000000000000000);
+        require(
+            toC[tokenAddr] != 0x0000000000000000000000000000000000000000 &&
+                toC[tokenAddr] != address(0) &&
+                toC[tokenAddr] != address(0x0)
+        );
 
         IERC20 token = IERC20(tokenAddr);
         IERC20Metadata tokenMetadata = IERC20Metadata(tokenAddr);
         CErc20 cToken = CErc20(toC[tokenAddr]);
-        ILendingPool pool = ILendingPool(
-            0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9
-        ); // AAVE LendingPool address
+        ILendingPool pool = ILendingPool(ILENDINGPOOL_ADDR); // AAVE LendingPool address
 
         // Interest rate formula adjusted to directly compare compound vs aave
+        /*
+          In Compound, interest rate APY is calculated with the formula
+          ((((Rate / ETH Mantissa * Blocks Per Day + 1) ^ Days Per Year)) - 1).
+          In AAVE, the formula is
+          ((1 + ((liquidityRate / RAY) / SECONDS_PER_YEAR)) ^ SECONDS_PER_YEAR) - 1.
+          We simplify the inequality between the two formula by altering 
+          the compounding term, making days per year to seconds per year 
+          or vice versa. This affects the final APY trivially. This allows
+          the terms in both sides to cancel, eventually becoming
+          compoundSupplyRate * 6570 * 365 * (10 ** 9) ? aaveLiquidityRate.
+        */
         uint256 cInterestAdj = cToken.supplyRatePerBlock() *
             6570 *
             365 *
--- ethereum_contracts/test/lending_optimizer.js
+++ ethereum_contracts/test/lending_optimizer.js
@@ -36,7 +36,16 @@ async function deployLendingOptimizer(signer) {
     signer
   );
 
-  const lendingOptimizer = await LendingOptimizer.deploy();
+  const lendingOptimizer = await upgrades.deployProxy(
+    LendingOptimizer,
+    [
+      "0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5",
+      "0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9",
+      "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+      "0xcc9a0B7c43DC2a5F023Bb9b738E45B0Ef6B06E04",
+    ], { unsafeAllow: ["delegatecall"], kind: "uups" }
+  );
+
   await lendingOptimizer.deployed();
 
   return lendingOptimizer;
@@ -142,6 +151,21 @@ describe.only("LendingOptimizer supply unit tests", function () {
   beforeEach("Setup before each test", async function () {
     signer = await getImpersonatedSigner();
     lendingOptimizer = await deployLendingOptimizer(signer);
+
+    await lendingOptimizer.mapToC("0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9", "0xe65cdB6479BaC1e22340E4E755fAE7E509EcD06c"); // AAVE
+    await lendingOptimizer.mapToC("0x0D8775F648430679A709E98d2b0Cb6250d2887EF", "0x6C8c6b02E7b2BE14d4fA6022Dfd6d75921D90E4E"); // BAT
+    await lendingOptimizer.mapToC("0x6B175474E89094C44Da98b954EedeAC495271d0F", "0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643"); // DAI
+    await lendingOptimizer.mapToC("0x956F47F50A910163D8BF957Cf5846D573E7f87CA", "0x7713DD9Ca933848F6819F38B8352D9A15EA73F67"); // FEI
+    await lendingOptimizer.mapToC("0x514910771AF9Ca656af840dff83E8264EcF986CA", "0xFAce851a4921ce59e912d19329929CE6da6EB0c7"); // LINK
+    await lendingOptimizer.mapToC("0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2", "0x95b4eF2869eBD94BEb4eEE400a99824BF5DC325b"); // MKR
+    await lendingOptimizer.mapToC("0x0000000000085d4780B73119b644AE5ecd22b376", "0x12392F67bdf24faE0AF363c24aC620a2f67DAd86"); // TUSD
+    await lendingOptimizer.mapToC("0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984", "0x35A18000230DA775CAc24873d00Ff85BccdeD550"); // UNI
+    await lendingOptimizer.mapToC("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", "0x39AA39c021dfbaE8faC545936693aC917d5E7563"); // USDC
+    await lendingOptimizer.mapToC("0x8E870D67F660D95d5be530380D0eC0bd388289E1", "0x041171993284df560249B57358F931D9eB7b925D"); // USDP
+    await lendingOptimizer.mapToC("0xdAC17F958D2ee523a2206206994597C13D831ec7", "0xf650C3d88D12dB855b8bf7D11Be6C55A4e07dCC9"); // USDT
+    await lendingOptimizer.mapToC("0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599", "0xC11b1268C1A384e55C48c2391d8d480264A3A7F4"); // WBTC
+    await lendingOptimizer.mapToC("0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e", "0x80a2AE356fc9ef4305676f7a3E2Ed04e12C33946"); // YFI
+    await lendingOptimizer.mapToC("0xE41d2489571d322189246DaFA5ebDe1F4699F498", "0xB3319f5D18Bc0D84dD1b4825Dcde5d5f7266d407"); // ZRX
   });
 
   // no longer works because supplyTokenToCompound is private
@@ -160,7 +184,7 @@ describe.only("LendingOptimizer supply unit tests", function () {
     expect(aDelta).to.equal(20);
   });
 
-  it.skip("Supply optimize between Compound and Aave, USDC", async function () {
+  it("Supply optimize between Compound and Aave, USDC", async function () {
     // at block 14957690, compound interst rate was around 0.60% APY, aave was around 1.34% APY, date 6/13/2022
     // the calculated interest rate is quite accurate, good enough and can't test for other dates as historical interset rate data cannot be found
     const [signerBalanceDelta, compoundBalanceDelta, cDelta, aaveBalanceDelta, aDelta] = await testSupplyUSDC(signer, lendingOptimizer);
@@ -171,7 +195,7 @@ describe.only("LendingOptimizer supply unit tests", function () {
     expect(aDelta).to.equal(20);
   });
 
-  it.skip("Supply optimize between Compound and Aave, USDT", async function () {
+  it("Supply optimize between Compound and Aave, USDT", async function () {
     // at block 14957690, compound had higher APY than aave for USDT
     const amount = 20 * 1e6; // $20
     const tokenContract = new ethers.Contract("0xdAC17F958D2ee523a2206206994597C13D831ec7", erc20ABI, signer);
@@ -195,7 +219,7 @@ describe.only("LendingOptimizer supply unit tests", function () {
     expect(Math.ceil(cDelta)).to.equal(20);
   });
 
-  it.skip("Supply rest of the tokens", async function () {
+  it("Supply rest of the tokens", async function () {
     await testSupplyOthers("0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599", signer, lendingOptimizer); // WBTC
     await testSupplyOthers("0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9", signer, lendingOptimizer); // AAVE
     await testSupplyOthers("0x0D8775F648430679A709E98d2b0Cb6250d2887EF", signer, lendingOptimizer); // BAT
@@ -211,12 +235,12 @@ describe.only("LendingOptimizer supply unit tests", function () {
     await testSupplyOthers("0xE41d2489571d322189246DaFA5ebDe1F4699F498", signer, lendingOptimizer); // ZRX
   });
 
-  it.skip("Supply: invalid address", async function () {
-    await testSupplyOthers("0xe65cdB6479BaC1e22340E4E755fAE7E509EcD06c", signer, lendingOptimizer);
+  it("Supply: invalid address", async function () {
+    await testSupplyOthers("0x0", signer, lendingOptimizer);
+    // await testSupplyOthers("0xe65cdB6479BaC1e22340E4E755fAE7E509EcD06c", signer, lendingOptimizer);
   });
 
   it("Supply ETH", async function () {
-    // const WETHContract = new ethers.Contract(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, erc20ABI, signer);
     const cETHContract = new ethers.Contract("0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5", cTokenAbi, signer);
     const aWETHContract = new ethers.Contract("0x030bA81f1c18d280636F32af80b9AAd02Cf0854e", aTokenAbi, signer);
 
