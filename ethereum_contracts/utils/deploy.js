40da58d045f79d38ac922a66c2d2625ed8542b5c
Clique <gaohan137@gmail.com>
2022-08-18 14:53:54 -0700

    Add deploy script for HomoraPDNVault and some refactoring

--- ethereum_contracts/utils/deploy.js
+++ ethereum_contracts/utils/deploy.js
@@ -1,9 +1,5 @@
 const { ethers } = require("hardhat");
 
-const wormholeTokenBridgeABI = [
-  "function wormhole() external view returns (address)",
-];
-
 async function deployApertureManager(signer, wormholeTokenBridgeAddr) {
   const curveRouterLib = await ethers.getContractFactory(
     "CurveRouterLib",
@@ -33,10 +29,7 @@ async function deployApertureManager(signer, wormholeTokenBridgeAddr) {
   const apertureManagerProxy = await upgrades.deployProxy(
     apertureManagerContractFactory,
     [
-      [
-        wormholeTokenBridgeAddr,
-        /*consistencyLevel=*/ 1,
-      ],
+      [wormholeTokenBridgeAddr, /*consistencyLevel=*/ 1],
       [/*feeBps=*/ 100, /*feeSink=*/ wormholeTokenBridgeAddr],
     ],
     { unsafeAllow: ["delegatecall"], kind: "uups" }
@@ -55,7 +48,69 @@ async function deployHomoraAdapter(signer) {
   return await homoraAdapterFactory.connect(signer).deploy();
 }
 
+async function deployHomoraPDNVault(signer, vaultConfig, txOptions) {
+  const {
+    wormholeTokenBridgeAddr,
+    controllerAddr,
+    tokenA,
+    tokenB,
+    homoraBankAddr,
+    spellAddr,
+    rewardTokenAddr,
+    poolId,
+  } = vaultConfig;
+  // Aperture manager contract.
+  const managerContract = await deployApertureManager(
+    signer,
+    wormholeTokenBridgeAddr
+  );
+  console.log("Aperture manager deployed at: ", managerContract.address);
+
+  // Deploy Homora adapter contract.
+  const homoraAdapter = await deployHomoraAdapter(signer);
+
+  // HomoraPDNVault contract.
+  var library = await ethers.getContractFactory("HomoraAdapterLib");
+  const adapterLib = await library.deploy();
+  library = await ethers.getContractFactory("VaultLib", {
+    libraries: {
+      HomoraAdapterLib: adapterLib.address,
+    },
+  });
+  const vaultLib = await library.deploy();
+  const strategyFactory = await ethers.getContractFactory("HomoraPDNVault", {
+    libraries: {
+      VaultLib: vaultLib.address,
+    },
+  });
+  const strategyContract = await upgrades.deployProxy(
+    strategyFactory,
+    [
+      managerContract.address,
+      homoraAdapter.address,
+      signer.address, // fee collector addr.
+      controllerAddr,
+      tokenA,
+      tokenB,
+      homoraBankAddr,
+      spellAddr,
+      rewardTokenAddr,
+      poolId,
+    ],
+    { unsafeAllow: ["delegatecall"], kind: "uups" }
+  );
+  await strategyContract.connect(signer).deployed(txOptions);
+  console.log(`HomoraPDNVault deployed at ${strategyContract.address}`);
+  return {
+    managerContract: managerContract,
+    strategyContract: strategyContract,
+    homoraAdapter: homoraAdapter,
+    vaultLib: vaultLib,
+  };
+}
+
 module.exports = {
   deployApertureManager: deployApertureManager,
   deployHomoraAdapter: deployHomoraAdapter,
+  deployHomoraPDNVault: deployHomoraPDNVault,
 };
